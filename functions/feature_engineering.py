33cebe99ce2acbfed0789f3fe7202aa2ff64d708
Matt Bolanos <matthew.a.bolanos@gmail.com>
2023-09-27 16:42:31 -0400

    added some features

--- functions/feature_engineering.py
+++ functions/feature_engineering.py
@@ -1,8 +1,9 @@
 # import libraries
 import pandas as pd
+import re
 
 
-def read_data(audio_path: str, artists_path: str, playlists_path: str):
+def get_data(audio_path: str, artists_path: str, playlists_path: str) -> list:
     """
     read in raw spotify api json data from local files.
 
@@ -29,4 +30,46 @@ def read_data(audio_path: str, artists_path: str, playlists_path: str):
         playlists = pd.read_json(f)
 
     # return list
+    return add_features(audio_features, artists, playlists)
+
+
+def add_features(audio_features: pd.DataFrame, artists: pd.DataFrame, playlists: pd.DataFrame) -> list:
+    """add features to read in spotify data
+
+    Args:
+        audio_features (pd.DataFrame): audio features
+        artists (pd.DataFrame): artists
+        playlists (pd.DataFrame): playlists
+    Returns:
+        audio_features (pd.DataFrame): audio features w/ added features
+        artists (pd.DataFrame): artists w/ added features
+        playlists (pd.DataFrame): playlists w/ added features
+    """
+    # --- playlists --- #
+    # add market ID to playlists
+    playlists["market_id"] = (
+        playlists["playlist_name"]
+        .apply(lambda x: re.sub(r"Top Songs - ", "", x))
+        .apply(lambda x: re.sub(r"\s+", "_", x).lower())
+    )
+
+    # add # of available markets by counting ","
+    # it is a list column so we need to make it a string first
+    playlists["num_avail_markets"] = playlists["available_markets"].apply(lambda x: len(str(x).split(",")))
+
+    # count # of artists per song
+    playlists["num_artists"] = playlists["artist_ids"].apply(lambda x: sum(1 for item in x if "id" in item))
+
+    # create rank variable, which is row number grouped by market_id
+    playlists["playlist_rank"] = playlists.groupby("market_id").cumcount() + 1
+
+    # drop some columns
+    playlists.drop(columns=["available_markets", "playlist_name"], inplace=True)
+
+    # rename some columns
+    playlists.rename(columns={"popularity": "track_popularity"}, inplace=True)
+
+    # -- artists -- #
+    artists.rename(columns={"popularity": "artist_popularity"}, inplace=True)
+
     return audio_features, artists, playlists
